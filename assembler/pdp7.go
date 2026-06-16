1a9d233b112445b79a0e897cbef1d877936d6e22
OniOkami666 <chompybird75@gmail.com>
2026-06-16 10:13:34 -0700

    updated

--- assembler/pdp7.go
+++ assembler/pdp7.go
@@ -1,48 +1,43 @@
 package pdp7
 
-import (
-	'encoding/binary',
-	'math/bits',
-	'strings'
-	'bufio'
-	'unicode'
-)
-
-type TokenType int 
+type TokenType int
 
 type Word18 uint32 // Go does not have a standard 18 bit integer type
 type Word13 uint16
 type Word4 uint8
 
 type PDP7 struct { // The CPU registers
-	AC Word18
-	PC Word13
+	AC   Word18
+	PC   Word13
 	Link bool
-	MAR Word13
-	IR Word4
+	MAR  Word13
+	IR   Word18
 }
 
 const (
-	OP_MASK = 0740000
-	INDIRECT = 0020000
-	ADDR_MASK = 0017777
-	OPR_BASE = 0740000
-	CLA = 0000200
-	CMA = 0001000
-	HLT = 0000002
+	OP_MASK   Word18 = 0o740000
+	INDIRECT  Word18 = 0o020000
+	ADDR_MASK Word18 = 0o017777
+
+	// Operate Instructions
+	OPR_BASE Word18 = 0o740000
+	CLA      Word18 = 0o000200
+	CMA      Word18 = 0o001000
+	HLT      Word18 = 0o000002
 )
-instructions := map[string]Word18{ 
-	'cal': 000000, // Call subroutine
-	'dac': 040000, // Deposit accumulator
-	'jms': 100000, // Jump to subroutine
-	'dzm': 140000, // Deposit zero in memory
-	'lac': 200000, // Load accumulator
-	'xor': 240000, // Exclusive OR (Boolean)
-	'add': 300000, // Add, 1's complement
-	'tad': 340000, // Add, 2's complement
-	'xct': 400000, // Execute
-	'isz': 440000, // Increment and skip if zero
-	'and': 500000, // Boolean and
-	'sad': 540000, // Skip if AC different from memory
-	'jmp': 600000, // Jump
+
+var instructions = map[string]Word18{
+	"cal": 0o000000, // Call subroutine
+	"dac": 0o040000, // Deposit accumulator
+	"jms": 0o100000, // Jump to subroutine
+	"dzm": 0o140000, // Deposit zero in memory
+	"lac": 0o200000, // Load accumulator
+	"xor": 0o240000, // Exclusive OR (Boolean)
+	"add": 0o300000, // Add, 1's complement
+	"tad": 0o340000, // Add, 2's complement
+	"xct": 0o400000, // Execute
+	"isz": 0o440000, // Increment and skip if zero
+	"and": 0o500000, // Boolean and
+	"sad": 0o540000, // Skip if AC different from memory
+	"jmp": 0o600000, // Jump
 }
